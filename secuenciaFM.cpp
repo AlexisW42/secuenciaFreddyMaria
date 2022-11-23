15a9f4efcdc4c96205489ab8aabe0c61e70f241c
Alexis Guerrero <gerreroalexis063@gmail.com>
2022-11-23 02:40:07 -0400

    leido numero de la primera linea

--- secuenciaFM.cpp
+++ secuenciaFM.cpp
@@ -6,10 +6,14 @@ using namespace std;
 int main() {
     ifstream file("secuencias.txt");
     string text;
+    int numbersOfLines;
+
     if (file.fail()) {
         cout << "Error!" << endl;
         return 0;
     }
+    file>>numbersOfLines;
+    cout << numbersOfLines << endl;
     while (!file.eof()) {
         getline(file, text);
         cout << text << endl;
