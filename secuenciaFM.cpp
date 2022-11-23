b345cd5a0350d10ee287ebcb04abe91ef898c3bd
Alexis Guerrero <gerreroalexis063@gmail.com>
2022-11-23 14:09:39 -0400

    refactorizacion en la toma de datos del archivo y creacion de arrays

--- secuenciaFM.cpp
+++ secuenciaFM.cpp
@@ -1,7 +1,11 @@
 #include <fstream>
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+void readLines(ifstream &file, int numbersOfLines);
+vector<int> readItems(ifstream &file, int numberOfItems);
 
 int main() {
     ifstream file("secuencias.txt");
@@ -15,19 +19,32 @@ int main() {
     file >> numbersOfLines;
     cout << "numbersOfLines " << numbersOfLines << endl;
 
-    for (int i = 0; i < numbersOfLines; i++) {
-        int num;
-        file >> num;
-        cout << num << " ";
-        for (int i = 0; i < num; i++) {
-            int numero;
-            file >> numero;
-            cout << numero << " ";
-        }
-        cout << endl;
-    }
+    readLines(file, numbersOfLines);
 
     file.close();
 
     return 0;
+}
+
+void readLines(ifstream &file, int numbersOfLines) {
+    for (int i = 0; i < numbersOfLines; i++) {
+        int numberOfItems;
+        vector<int> line;
+        file >> numberOfItems;
+        cout << numberOfItems << " ";
+        line = readItems(file, numberOfItems);
+        cout << endl;
+        
+    }
+}
+
+vector<int> readItems(ifstream &file, int numberOfItems) {
+    vector<int> line;
+    for (int i = 0; i < numberOfItems; i++) {
+        int number;
+        file >> number;
+        line.push_back(number);
+        cout << number << " ";
+    }
+    return line;
 }
