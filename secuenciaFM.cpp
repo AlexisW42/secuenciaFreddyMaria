99e95bb2487b6f3c3b5a975576b4b0b932a0ca0f
Alexis Guerrero <gerreroalexis063@gmail.com>
2022-11-23 02:06:36 -0400

    Agregado lectura de archivo

--- secuenciaFM.cpp
+++ secuenciaFM.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main() {
+    ifstream file("secuencias.txt");
+    string text;
+    if (file.fail()) {
+        cout << "Error!" << endl;
+        return 0;
+    }
+    while (!file.eof()) {
+        getline(file, text);
+        cout << text << endl;
+    }
+
+    file.close();
+
 	int a=5;
 	int b=2;
 	
